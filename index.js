013179f16483dd77c62a95cfac753010724d38b9
bhanu2003-bh <84559130+bhanu2003-bh@users.noreply.github.com>
2023-09-15 23:29:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var app = express();
+var port = 3000;
+var flag = false;
+app.use(bodyParser.urlencoded({extended:true}));
+
+function checker(req,res,next){
+   if(req.body.password=="iloveyou"){
+    flag =  true;
+   }
+    next();
+}
+app.use(checker);
+
+app.get("/", (req,res)=>{
+res.sendFile(__dirname+"/public/index.html");
+})
+
+app.post("/check",checker,(req,res) =>{
+if(flag){
+    res.sendFile(__dirname+"/public/secret.html");
+}
+else{
+    res.sendFile(__dirname+"/public/index.html");
+}
+})
+
+app.listen(port,(req,res) =>{
+    console.log("Server is working at.."+port);
+})
